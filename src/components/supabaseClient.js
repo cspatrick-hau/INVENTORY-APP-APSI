331ed6d5364ef458f614db0700b74617216be32a
agcamartin <agcamartin@student.hau.edu.ph>
2025-09-20 17:39:55 +0800

    Update supabaseClient.js

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ⚠️ Replace with your Supabase project credentials
-const supabaseUrl = "https://nguouseiyuzuvzqwmmwx.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndW91c2VpeXV6dXZ6cXdtbXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwOTkyMjIsImV4cCI6MjA3MzY3NTIyMn0.s9i6_JSaYOgrBQTWnesr7dwHMalV514yjf2L3YguEyE"
+const SUPABASE_URL = "https://nguouseiyuzuvzqwmmwx.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndW91c2VpeXV6dXZ6cXdtbXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwOTkyMjIsImV4cCI6MjA3MzY3NTIyMn0.s9i6_JSaYOgrBQTWnesr7dwHMalV514yjf2L3YguEyE"
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
